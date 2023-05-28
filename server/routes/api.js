4d2b39046e7ce5e408682a1da947dbc14df62c85
EunJin.Kim <jennyeunjin@khu.ac.kr>
2023-05-28 19:23:59 +0900

    Add Signup emailAuth api

--- server/routes/api.js
+++ server/routes/api.js
@@ -4,9 +4,11 @@ var router = express.Router();
 const auth = require('../controller/auth.js');
 const db = require('../config/db.js');
 const sess = require('../controller/sessionChecker.js');
+const emailAuth=require('../controller/emailAuth.js');
 
 
 // 회원 가입 요청 처리
+// 리액트에서는 이후에 email 인증코드 확인하는 페이지로 연결해주어야함!!
 router.post('/signup', (req, res) => {
   const { id, name, email, password } = req.body;
   const user = { id, name, email, password };
@@ -15,21 +17,44 @@ router.post('/signup', (req, res) => {
     console.log('user: ', req.body.id + ' - sign up fail: invalid input data');
     res.json({ result: 'fail', msg: "유효하지 않은 입력입니다." });
   }
-  db.query('SELECT * FROM User WHERE id=?', [id], (err, data) => {
-    if (data.length == 0) { // DB에 해당 id에 대한 정보가 없을 경우
-      db.query('INSERT INTO User SET ?', user, (err, result) => {
-        if (err) {
-          throw err;
+  db.query('SELECT * FROM User WHERE id=?', [id], (err, idData) => {
+    if(err){ throw err; }
+    if (idData.length == 0) { // DB에 해당 id에 대한 정보가 없을 경우
+      db.query('SELECT * FROM User WHERE email=?', [email], (err, mailData) => { // DB에 해당 email 정보있는지 검색
+        if (err) { throw err; }
+        if (mailData.length == 0) { //DB에 해당 email에 대한 정보 없는 경우 -> 가입 진행
+          // 이메일 인증 절차
+          req.session.usrEmail = user.email;
+          req.session.user = user; // post(/signupAuth)에서 db에 정보 넣기 위해 세션에 user 정보 저장...
+          emailAuth.emailAuthentication(req, res);
+        }else{ // 이미 사용중인 email인 경우
+          console.log('user: ', req.body.email + ' - sign up fail: email already exist')
+          res.json({result:'fail',msg:"이미 사용중인 이메일 입니다."});
         }
       })
-      console.log('user: ', req.body.id + ' - sign up success');
-      res.json({ result: 'success', msg: "회원가입에 성공했습니다." });
     } else { // DB에 중복된 id가 있을 경우
       console.log('user: ', req.body.id + ' - sign up fail: ID already exist');
       res.json({ result: 'fail', msg: "이미 사용중인 ID 입니다." });
     }
   })
 });
+
+router.post('/signupAuth',(req,res)=>{
+  const usrInput = Number(req.body.authCode);
+  console.log("usr Input: " + usrInput + ", session value: " + req.session.authCode);
+  if (usrInput === req.session.authCode) { // 인증코드 일치하는 경우, 회원가입 처리
+    db.query('INSERT INTO User SET ?', req.session.user, (err, result) => {
+      if (err) {
+        throw err;
+      }
+    })
+    console.log('user: ', req.session.user.id + ' - sign up success');
+    delete req.session.user; // db에 입력하기 위해 임시로 저장한 req.session.user 삭제
+    res.json({result:"success",msg:"회원가입에 성공했습니다."});
+  } else { // 인증코드 불일치
+    res.json({result:'fail',msg:'인증코드가 일치하지 않습니다.'})
+  }
+})
 
 // 로그인 요청 처리
 router.post('/login', (req, res) => {
